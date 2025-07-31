00f5f00ae128dbb45f389706ff91ffd22c3d1ae5
Simon Dirmeier <simd23@protonmail.com>
2025-07-31 20:03:29 +0200

    Update evaluation

---
+++ experiments/evaluate_latent_edm-train.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+#SBATCH --nodes=1
+#SBATCH --time=23:59:59
+#SBATCH --partition=normal
+#SBATCH --account=sd28
+#SBATCH --output=/capstor/scratch/cscs/sdirmeie/PROJECTS/highfem/src/tqdne/experiments/workdir/slurm/%j.out
+#SBATCH --error=/capstor/scratch/cscs/sdirmeie/PROJECTS/highfem/src/tqdne/experiments/workdir/slurm/%j.out
+
+
+conda activate /capstor/scratch/cscs/sdirmeie/PROJECTS/highfem/workdir/envs/tqdne-dev
+
+srun torchrun --nproc_per_node=1 evaluate.py \
+    --split=train \
+    --workdir=/capstor/scratch/cscs/sdirmeie/PROJECTS/highfem/src/tqdne/experiments/workdir \
+    --edm_checkpoint=/capstor/scratch/cscs/sdirmeie/PROJECTS/highfem/src/tqdne/experiments/workdir/outputs/edm.ckpt \
+    --autoencoder_checkpoint=/capstor/scratch/cscs/sdirmeie/PROJECTS/highfem/src/tqdne/experiments/workdir/outputs/autoencoder.ckpt \
+    --classifier_checkpoint=/capstor/scratch/cscs/sdirmeie/PROJECTS/highfem/src/tqdne/experiments/workdir/outputs/Classifier-LogSpectrogram/best.ckpt
